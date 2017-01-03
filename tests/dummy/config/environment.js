ed59b97ed77c20b83c11e3b87480d70e14e40f8b
Kilowhisky <chrisrice@san.rr.com>
2017-01-03 09:34:52 -0800

    Upgrade dependencies to ember 2.10.*

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,19 +4,22 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
       }
     },
 
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-      // TODO add flags/options to control which demo template is loaded
     }
   };
 
@@ -30,7 +33,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
